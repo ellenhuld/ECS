01dba63550d25d4315ef67e71bf247148325cad9
Ellen Huld Thordardottir <au582001@uni.au.dk>
2020-02-12 15:54:03 +0100

    FakeHeater beskrivelse

--- ECSTest.Unit/FakeHeater.cs
+++ ECSTest.Unit/FakeHeater.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ECS.Legacy;
 
 namespace ECSTest.Unit
 {
-    public class FakeHeate : IHeater
+    public class FakeHeater : IHeater
     {
+        public void TurnOn()
+        {
+            //count++;
+        }
 
+        public void TurnOff()
+        {
+            System.Console.WriteLine("Heater is off");
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
     }
 }
