8207eeff546b4215f7b65842b03713a3accb5eb2
KasperFogh <44428137+KasperFogh@users.noreply.github.com>
2020-02-12 15:54:55 +0100

    virker

--- ECSTest.Unit/FakeHeater.cs
+++ ECSTest.Unit/FakeHeater.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ECS.Legacy;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,19 @@ namespace ECSTest.Unit
 {
     public class FakeHeate : IHeater
     {
+        public void TurnOn()
+        {
+            System.Console.WriteLine("Heater is on");
+        }
 
+        public void TurnOff()
+        {
+            System.Console.WriteLine("Heater is off");
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
     }
 }
