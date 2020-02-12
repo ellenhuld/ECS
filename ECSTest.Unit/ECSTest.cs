4de8893b0726287b7c7c11ab89c00ff1721a0ae9
Ellen Huld Thordardottir <au582001@uni.au.dk>
2020-02-12 15:20:24 +0100

    TestClass

---
+++ ECSTest.Unit/ECSTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ECS.Legacy;
+
+
+namespace ECSTest.Unit
+{
+    [TestFixture]
+    public class ECSTest
+    {
+        private TempSensor _uut;
+
+        [SetUp]
+        {
+            _uut = new TempSensor();
+        } 
+    }
+}
